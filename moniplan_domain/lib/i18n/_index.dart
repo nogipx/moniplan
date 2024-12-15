e4d7fac5624d807e739a5e28c94a42b3f2d80188
nogipx <nogipx@gmail.com>
2024-12-15 23:40:38 +0300

    fix: move keys to i18n

---
+++ moniplan_domain/lib/i18n/_index.dart
@@ -0,0 +1,1 @@
+export 'moniplan.keys.dart';
--- moniplan_domain/lib/moniplan_domain.dart
+++ moniplan_domain/lib/moniplan_domain.dart
@@ -1,8 +1,9 @@
 library moniplan_domain;
 
-export 'src/_index.dart';
-export 'log.dart';
-
+export 'package:intl/intl.dart';
 export 'package:money2/money2.dart';
 export 'package:uuid/uuid.dart';
-export 'package:intl/intl.dart';
+
+export 'i18n/_index.dart';
+export 'log.dart';
+export 'src/_index.dart';
